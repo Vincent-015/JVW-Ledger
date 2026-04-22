8bb6211f3a29bd1d278793a1b540b15b29622186
unknown <UU324@ccs.local>
2026-04-22 16:10:39 +0800

    Functional/Stretched UI

--- obj/Debug/net8.0/FinLedger.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinLedger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinLedger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1a00134e8da8779c6d7efa24b28d331e2c1813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8326138cd65c4ceebf198b99b3643ffdc6a559")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinLedger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinLedger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
